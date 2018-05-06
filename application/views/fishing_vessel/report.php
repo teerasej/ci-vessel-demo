471cdef9336a27325dbfb506796921700acbd58c
Teerasej <teerasej@gmail.com>
2018-05-06 20:06:45 +0700

    สร้าง layout สำหรับ chart แรก

--- application/views/fishing_vessel/report.php
+++ application/views/fishing_vessel/report.php
@@ -1,1 +1,14 @@
 <script src="<?=base_url(); ?>assets/js/report.js"></script>
+<div class="row">
+    <div class="col-6">
+        <div class="card">
+            <div class="card-header">
+                จำนวนเรือแยกตามประเทศ
+            </div>
+            <div class="card-body">
+                <div id="barchart"></div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6"></div>
+</div>
