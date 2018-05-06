8088d8c5b0bdf7164ab21df8759a8a2301383c42
Teerasej <teerasej@gmail.com>
2018-05-06 22:17:50 +0700

    สร้าง method สำหรับ group ค่าจำนวนเรือแต่ละประเทศ

--- application/models/FishingVessel_model.php
+++ application/models/FishingVessel_model.php
@@ -1,10 +1,9 @@
 <?php
 
+defined('BASEPATH') or exit('No direct script access allowed');
 
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Fishingvessel_model extends CI_Model {
-
+class Fishingvessel_model extends CI_Model
+{
 
     public function __construct()
     {
@@ -12,13 +11,12 @@ class Fishingvessel_model extends CI_Model {
         $this->load->database();
     }
 
-
     public function getAll()
     {
-        
-        $this->db->select('v.id as id, v.Name as VesselName, v.imageUrl as imageUrl, c.Name as CountryName',false);
+
+        $this->db->select('v.id as id, v.Name as VesselName, v.imageUrl as imageUrl, c.Name as CountryName', false);
         $this->db->from('Vessel as v');
-        $this->db->join('Country as c','v.Country_ID = c.id');
+        $this->db->join('Country as c', 'v.Country_ID = c.id');
 
         // order by
         $this->db->order_by('v.id', 'DESC');
@@ -28,28 +26,35 @@ class Fishingvessel_model extends CI_Model {
 
         // get all rows...
         // $query = $this->db->get('Vessel');
-        
+
+        return $query->result_array();
+    }
+
+    public function all_vessels_group_by_country()
+    {
+        $this->db->select('c.Name as CountryName, COUNT(c.Name) as CountryTotal', false);
+        $this->db->from('Vessel as v');
+        $this->db->group_by('c.Name');
+        $this->db->join('Country as c', 'v.Country_ID = c.id');
+
+        $query = $this->db->get();
         return $query->result_array();
     }
 
     public function saveVessel($uploaded_image_path = null)
     {
-        if(is_null($uploaded_image_path))
-        {
-            $data = array(
-                'Name' => $this->input->post('vesselName'),
-                'Country_ID' => (int) $this->input->post('country')
-            );
-        }
-        else 
-        {
+        if (is_null($uploaded_image_path)) {
             $data = array(
                 'Name' => $this->input->post('vesselName'),
                 'Country_ID' => (int) $this->input->post('country'),
-                'imageUrl' => $uploaded_image_path
+            );
+        } else {
+            $data = array(
+                'Name' => $this->input->post('vesselName'),
+                'Country_ID' => (int) $this->input->post('country'),
+                'imageUrl' => $uploaded_image_path,
             );
         }
-       
 
         return $this->db->insert('Vessel', $data);
     }
@@ -57,7 +62,7 @@ class Fishingvessel_model extends CI_Model {
     public function remove_vessel()
     {
         $data = array(
-            'id' => $this->input->post('id')
+            'id' => $this->input->post('id'),
         );
         return $this->db->delete('Vessel', $data);
     }
@@ -65,6 +70,3 @@ class Fishingvessel_model extends CI_Model {
 }
 
 /* End of file Fishingvessel_model.php */
-
-
-?>
