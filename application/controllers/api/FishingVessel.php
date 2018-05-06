cc3b0cac2f03216c4a6750c56a8b1e574fcca6d7
Teerasej <teerasej@gmail.com>
2018-05-06 19:53:18 +0700

    โหลดข้อมูลเรือประมงทั้งหมด และแสดงเป็นแบบ json

--- application/controllers/api/FishingVessel.php
+++ application/controllers/api/FishingVessel.php
@@ -5,9 +5,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class FishingVessel extends CI_Controller {
 
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('fishingvessel_model');
+        $this->load->model('country_model');
+    }
+
     public function index()
     {
         echo 'json';
+    }
+
+    public function all_vessels()
+    {
+        $data['vessels'] = $this->fishingvessel_model->getAll();
+
+        echo json_encode($data['vessels']);
     }
 
 }
