c37ae49573ddfdd7de33f683ac2b7d205f41b888
Teerasej <teerasej@gmail.com>
2018-05-06 22:18:32 +0700

    ส่ง Request ไปยัง api และเอาข้อมูลมาเรียงเพื่อให้ตรงกับที่ Fusionchart ต้องการ

--- assets/js/report.js
+++ assets/js/report.js
@@ -1,3 +1,43 @@
+
+
 $(document).ready(function () {
-    alert('ok');
+    
+    
+    $.get('http://localhost:8888/app-admin/index.php/api/fishingvessel/all_vessels', function(result){
+        console.log(result);
+
+        var resultArray = JSON.parse(result);
+        console.log(resultArray);
+
+        var data = [];
+
+
+        $.each(resultArray, function(index, item){
+            data.push({
+                label: item.CountryName
+                , value: item.CountryTotal
+            });
+        });
+
+        $("#barchart").insertFusionCharts({
+            type: "column2d",
+            width: "100%",
+            height: "400",
+            dataFormat: "json",
+            dataSource: {
+                "chart": {
+                    "caption": "",
+                    "xAxisName": "ประเทศ",
+                    "yAxisName": "จำนวน/ลำ",
+                    "theme": "fint"
+                },
+                "data": data
+            }
+
+        });
+    });
+
+    
+    
+
 });
