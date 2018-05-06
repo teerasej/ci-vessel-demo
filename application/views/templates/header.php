155dfd03febfc4fdaceee57d8d3f7a7f3d508f89
Teerasej <teerasej@gmail.com>
2018-05-06 21:52:46 +0700

    เพิ่ม jquery 3.3.1

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,19 +4,18 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
         
+        <script src="<?=base_url();?>assets/js/chart/fusioncharts.js"></script>
+        <script src="<?=base_url();?>assets/js/chart/fusioncharts.charts.js"></script>
+        <script src="<?=base_url();?>assets/js/chart/themes/fusioncharts.theme.fint.js"></script>
+        <script
+			  src="https://code.jquery.com/jquery-3.3.1.js"
+			  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+			  crossorigin="anonymous"></script>
+        <script src="<?=base_url();?>assets/js/fusioncharts-jquery-plugin.js"></script>
         
-
-        <script src="<?=base_url(); ?>assets/js/chart/fusioncharts.js"></script>
-        <script src="<?=base_url(); ?>assets/js/chart/fusioncharts.charts.js"></script>
-        <script src="<?=base_url(); ?>assets/js/chart/themes/fusioncharts.theme.fint.js"></script>
-
-        <script src="<?=base_url(); ?>assets/js/jquery-3.3.1.min.js"></script>
-        <script src="<?=base_url(); ?>assets/js/fusioncharts-jquery-plugin.min.js"></script>
-
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-        <link rel="stylesheet" href="<?=base_url(); ?>assets/css/style.css"/>
+        <link rel="stylesheet" href="<?=base_url();?>assets/css/style.css"/>
         <title><?php echo $title ?></title>
 </head>
 <body>
@@ -41,4 +40,3 @@
   </div>
 </nav>
 <div class="container">
-        
